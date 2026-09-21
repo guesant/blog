64e080e882cdc8d0d13133c7953cca1d3f25fb43
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2026-09-21 16:55:31 -0300

    fix(laravel): enable multiple production workers

--- src/laravel/docker/entrypoint.prod.sh
+++ src/laravel/docker/entrypoint.prod.sh
@@ -5,4 +5,4 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-exec php artisan serve --host=0.0.0.0 --port=8000
+exec php artisan serve --no-reload --host=0.0.0.0 --port=8000

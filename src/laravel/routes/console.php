4140fd5305862f6c7fd0bd314b3d7d95b7af8125
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2026-09-22 16:19:54 -0300

    chore(laravel): disable scheduled jobs

--- src/laravel/routes/console.php
+++ src/laravel/routes/console.php
@@ -1,22 +1,8 @@
 <?php
 
-use App\Jobs\WarmPublicSiteChrome;
-use App\Jobs\WarmResourceFacetCache;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::command('content:sync-dependency-credits')->weekly();
-Schedule::job(new WarmResourceFacetCache)->everyFiveMinutes()->withoutOverlapping();
-Schedule::job(new WarmPublicSiteChrome('en'))
-    ->name('warm-public-site-chrome-en')
-    ->everyFiveMinutes()
-    ->withoutOverlapping();
-Schedule::job(new WarmPublicSiteChrome('pt-BR'))
-    ->name('warm-public-site-chrome-pt-BR')
-    ->everyFiveMinutes()
-    ->withoutOverlapping();
